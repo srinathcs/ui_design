4eaa4ec1ba4002b016eaa84daf78a2c3eadb8696
Srinath <srinath001me@gmail.com>
2022-07-22 11:24:18 +0530

    ui design

--- app/src/main/java/com/facebook/design/MainActivity.java
+++ app/src/main/java/com/facebook/design/MainActivity.java
@@ -2,13 +2,20 @@ package com.facebook.design;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+ImageView imageView;
+Drawable drawable;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView=findViewById(R.id.img);
+        imageView.setImageResource(R.drawable.imv);
+        getSupportActionBar().hide();
     }
 }
