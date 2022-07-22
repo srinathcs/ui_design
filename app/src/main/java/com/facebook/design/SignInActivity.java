32ca434eba0cc4cc4752370ffecdb02ddb32893e
Srinath <srinath001me@gmail.com>
2022-07-22 15:10:08 +0530

    name changes

--- app/src/main/java/com/facebook/design/SignInActivity.java
+++ app/src/main/java/com/facebook/design/SignInActivity.java
@@ -6,13 +6,13 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class SignInActivity extends AppCompatActivity {
 ImageView imageView;
 Drawable drawable;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sign_in);
 
         imageView=findViewById(R.id.img);
         imageView.setImageResource(R.drawable.imv);
