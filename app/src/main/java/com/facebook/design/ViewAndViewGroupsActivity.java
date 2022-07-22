2ceeb1ea84f0dd60ba0512991a6590f12b4fd91f
Srinath <srinath001me@gmail.com>
2022-07-22 15:08:25 +0530

    name changes

--- app/src/main/java/com/facebook/design/ViewAndViewGroupsActivity.java
+++ app/src/main/java/com/facebook/design/ViewAndViewGroupsActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ViewAndViewGroupsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_view_view_gropus);
     }
 }
