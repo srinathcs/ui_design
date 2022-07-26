9822a614c1915181bfcd9175297e7bf722f10feb
Srinath <srinath001me@gmail.com>
2022-07-26 14:57:11 +0530

    image and activity impl

--- app/src/main/java/com/facebook/design/MainActivity.java
+++ app/src/main/java/com/facebook/design/MainActivity.java
@@ -2,7 +2,10 @@ package com.facebook.design;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,55 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button changeActivityBtn = findViewById(R.id.btn_1);
+        Button changeActivityBtn_2 = findViewById(R.id.btn_2);
+        Button changeActivityBtn_3 = findViewById(R.id.btn_3);
+        Button changeActivityBtn_4 = findViewById(R.id.btn_4);
+        changeActivityBtn_4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity5();
+            }
+        });
+        changeActivityBtn_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity4();
+            }
+        });
+        changeActivityBtn_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity3();
+            }
+        });
+
+        changeActivityBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity2();
+            }
+        });
+
+
+    }
+
+    public void openActivity2() {
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
+    }
+
+    public void openActivity3() {
+        Intent intent = new Intent(this, MainActivity3.class);
+        startActivity(intent);
+    }
+    public void openActivity4(){
+        Intent intent =new  Intent(this,MainActivity4.class);
+        startActivity(intent);
+    }
+    public void openActivity5(){
+        Intent intent = new Intent(this,MainActivity5.class);
+        startActivity(intent);
     }
 }
+
